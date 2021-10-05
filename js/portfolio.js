0d21f8afd2d107095dcb13c30478547dbaaae53a
Alexander Wh <111kanzas111@mail.ru>
2021-10-06 01:24:42 +0300

    Texts, process block styles

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,36 +4,42 @@ const projects = [
         description: 'Браузерное приложение погоды. Вёрстка на flex. Стили на SASS. Функционал на чистом JS. Определяет местоположение, есть выбор города из списка.',
         background: 'img/projects/weather.gif',
         projectlink: 'https://agamawd.github.io/weather/',
+        gpsLink: ''
     },
     {
         title: 'Lavakon Mix (тестовое задание)',
         description: 'Лендинг производства кондитерских смесей. Вёрстка на flex. Стили на чистом CSS. Мобильное меню-гамбургер на нативном на нативном JS.',
         background: 'img/projects/lavakon_mix.gif',
         projectlink: 'a98836qh.beget.tech/lavakonMix',
+        gpsLink: ''
     },
     {
         title: 'Аптечный препарат (тестовое задание)',
         description: 'Лендинг аптечного препарата. Вёрстка на flex. Стили на чистом CSS. Видеофон первого экрана. Анимация CSS. Слайдер и счётчик на нативном JS.',
         background: 'img/projects/level_up.gif',
         projectlink: 'http://a98836qh.beget.tech/level_up/',
+        gpsLink: ''
     },
     {
         title: 'Ремонт квартир (учебный проект)',
         description: 'Лендинг по ремонту квартир под ключ. Использовался Slick слайдер, библиотека wow.js, валидация форм и маска телефона на jQuery. Стили написаны на SASS. Настроена отправка формы через Ajax.',
         background: 'img/projects/repair.gif',
         projectlink: 'http://a98836qh.beget.tech',
+        gpsLink: ''
     },
     {
         title: 'Разработка приложений и сайтов (учебный проект)',
         description: 'Лендинг по разработке приложений и сайтов. Вёрстка на flex. Стили на чистом CSS. Использовался Slick слайдер, jQuery.',
         background: 'img/projects/marathon.gif',
         projectlink: 'http://a98836qh.beget.tech/Marathon/',
+        gpsLink: ''
     },
     {
         title: 'Email-письмо',
         description: 'По всем канонам вёрстки email-рассылок.',
         background: 'img/projects/email.gif',
         projectlink: 'http://a98836qh.beget.tech/email/',
+        gpsLink: ''
     },
 ]
 const numberOfProjects = projects.length
@@ -45,7 +51,7 @@ function createProject() {
         let title = projects[i].title
         let description = projects[i].description
         let projectlink = projects[i].projectlink
-        let githubLink = projects[i].githubLink
+        let gpsLink = 'https://developers.google.com/speed/pagespeed/insights/?url=' + projectlink
         let background = projects[i].background
         let htmlLayout = `<div class="portfolio__item--hover">
                             <div>
@@ -54,6 +60,7 @@ function createProject() {
                             </div>
                             <div>
                                 <a href="${projectlink}" target='_blank' class="item__link">Открыть сайт</a>
+                                <a href="${gpsLink}" target='_blank' class="item__link">Проверить скорость</a>
                             </div>
                         </div>`
         let portfolioItem = document.createElement('div')
