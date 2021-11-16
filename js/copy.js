bbac808484e477f3eb0887b7b040fe2a368ff65a
Alexander Wh <111kanzas111@mail.ru>
2021-11-16 23:54:59 +0300

    Configured copying of links. Open / close modal window is partially configured.

---
+++ js/copy.js
@@ -0,0 +1,16 @@
+const tooltip = document.createElement('div');
+tooltip.innerHTML = "<span style='display: block; color:rgb(238, 239, 241); font-size:0.9rem; line-height: 1.7; text-align: center; border-radius: 5px; width: 100px; background-color: rgb(50,50,50);'>Скопировано!</span>"
+
+async function copy(event, id) {
+    const text = document.getElementById(id).innerText
+    try {
+        await navigator.clipboard.writeText(text);
+        tooltip.style.position = 'fixed';
+        tooltip.style.top = `${event.clientY-45}px`;
+        tooltip.style.left = `${event.clientX-50}px`;
+        document.body.appendChild(tooltip);
+        setTimeout(() =>  {document.body.removeChild(tooltip)}, 600);
+      } catch (err) {
+        console.error('Ошибка копирования в буфер: ', err);
+      }
+}
---
+++ js/modal.js
@@ -0,0 +1,9 @@
+const modal = document.getElementById('js-modal')
+
+function openModal() {
+    modal.classList.remove('hidden')
+};
+
+function closeModal() {
+    modal.classList.add('hidden')
+};
